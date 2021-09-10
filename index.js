dc59670b4f7db080ebf978983fb0e7f1084a4f27
yashkumar0707 <43877972+yashkumar0707@users.noreply.github.com>
2021-09-10 18:45:42 +0530

    update on db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let mysql = require('mysql');
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'nitkfaps1234',
+    password: '',
     database: 'security'
 });
 
